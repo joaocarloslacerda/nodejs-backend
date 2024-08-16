14c72fed6fdaf9963249d87dac5349b3e8a79410
juniorlacerda <juniorlacerdasilva@gmail.com>
2024-08-15 21:59:57 -0300

    feat: listagem e inserção de departamento

--- api/src/controllers/departamentosControllers.ts
+++ api/src/controllers/departamentosControllers.ts
@@ -1,10 +1,29 @@
 import { Response, Request } from "express";
+import conexao from "../services/conection";
 
-export const detartamentosResponse = (req: Request, res: Response):void => {
+export const detartamentosResponse = async (req: Request, res: Response) => {
+
+  const [rows] = await conexao.query('SELECT * FROM DEPARTAMENTOS');
+  res.json(rows);
+
   res.send('GET departamentos');
 };
 
-export const departamentosRequest = (req: Request, res: Response):void => {
-  console.log(req.body);
-  res.send('POST departamentos');
+export const departamentosRequest = async (req: Request, res: Response) => {
+  const { nome, sigla } = req.body;
+
+  try{
+    const [result] = await conexao.execute(
+      'INSERT INTO DEPARTAMENTOS (sigla, nome) VALUES (? , ?)',
+      [sigla, nome]
+    );
+
+    res.status(201).json({
+      message: 'Departamento criado'
+    });
+  }catch (e) {
+    res.status(500).json({
+      message: 'Erro na criação'
+    })
+  }
 };
